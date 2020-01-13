197598bcbfb5e7936c7a022206595b75c22f55d4
arsen <arsen@LAPTOP-K7E9IAPG>
2020-01-13 15:47:37 +0900

    두번째 커밋

--- src/thisjava/ArrayCreateByValueListExample2.java
+++ src/thisjava/ArrayCreateByValueListExample2.java
@@ -3,8 +3,8 @@ package thisjava;
 public class ArrayCreateByValueListExample2 {
 	public static void main(String[] args) {
 		int[] scores;
-		scores = new int[] { 83, 90, 87 };
-
+		scores = new int[] { 84, 90, 87 };
+//		char c ='A';
 		int sum1 = 0;
 		for (int i = 0; i < 3; i++) {
 			sum1 += scores[i];
@@ -13,6 +13,7 @@ public class ArrayCreateByValueListExample2 {
 		System.out.println("총합 :" + sum1);
 		
 		int sum2 = add(new int[] {83,90,87});
+		
 	}
 
 	public static int add(int[] scores) {
