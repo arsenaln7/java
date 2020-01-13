6d468c70fc870511d6f5ad4297966e07fa46cba2
arsen <arsen@LAPTOP-K7E9IAPG>
2020-01-13 15:38:02 +0900

    첫번째 커밋

---
+++ src/thisjava/ArrayExample.java
@@ -0,0 +1,16 @@
+package thisjava;
+
+public class ArrayExample {
+ public static void main(String[] args) {
+	 String[] names=null;
+	 names= new String[] {"신용권","홍길동","감자바"};
+	 System.out.println(names[0]+names[1]+names[2]);
+	 names=null;
+	 
+	 String[] name;
+	 name = new String[] {"aa","ab"};
+	 
+	 System.out.println(name[0]+name[1]);
+	
+ }
+}
---
+++ src/thisjava/String_type.java
@@ -0,0 +1,19 @@
+package thisjava;
+//p145. String 타입
+public class String_type {
+ public static void main(String[] args) {
+//	 String name1="신용권";
+//	 String name2="신용권";
+//	 System.out.println(name1==name2);
+//	 
+//	 String name1 = new String("신용권");
+//	 String name2 = new String("신용권");
+	 
+	 String name1 = "신민철";
+	 String name2 = "신민철";
+	 String name3 = new String("신민철");
+	 System.out.println(name1==name2);
+	 System.out.println(name1==name3);
+	 
+ }
+}
